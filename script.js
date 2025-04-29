3d8c06e49939c03c43ee558ea3b91fc4d2af232f
MedAviTech12 <ajha1837@gmail.com>
2025-04-29 23:22:13 +0530

    mero project

--- script.js
+++
@@ -1,8 +0,0 @@
-// Highlight active tab
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', function() {
-      document.querySelectorAll('.nav-link').forEach(nav => nav.classList.remove('active'));
-      this.classList.add('active');
-    });
-  });
-  
